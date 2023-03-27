0f9e4a41d6c14b5166341841f2479cf1dc4afa1c
bouquet1 <bukuleta@gmail.com>
2023-03-27 15:24:31 -0700

    screenshots

--- index.js
+++ index.js
@@ -51,13 +51,13 @@ function calculatesFarePrice(start, destination){
 // function calculatesFarePrice(start, destination){
 //     const distance = distanceTravelledInFeet(start, destination);
 //     if (distance < 400) {
-//         return 0  // if travelledDistance < 400 = $0
+//         return 0  
 //        } else if (distance >= 400 && distance < 2000) {
-//         return (distance - 400) * 0.02  // if travelledDistance < 400 and > 2000 = 0.2 per foot
+//         return (distance - 400) * 0.02 
 //        }  else if (distance >= 2000 && distance < 2500) {
-//         return 25   // if travelledDistance < 2500 and > 2000 = $25 flat fare 
+//         return 25   
 //        } else {
-//         return "cannot travel that far"  // if travelledDistance > 2500 = 'cannot travel that far'
+//         return "cannot travel that far"  
 //        }
 //     }
 
